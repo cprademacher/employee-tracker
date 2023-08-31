85c7f9c2b7f003714fe3fec2dcafe4359b3541b6
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-08-31 09:59:39 -0500

    Added a connection to the database and added sql files

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
 // Import external tools
 const express = require("express");
 const inquirer = require("inquirer");
+const mysql = require("mysql2");
 
 // Import internal tools
 
 const app = express();
 
-const promptUser = () => {
-  return inquirer.prompt([
+// Connect to database
+const db = mysql.createConnection(
     {
-      type: "list",
-      message: "What would you like to do? ",
-      choices: [
-        "View all departments",
-        "View all roles",
-        "View all employees",
-        "Add a department",
-        "Add a role",
-        "Add an employee",
-        "Update an employee role",
-      ],
-      name: "options",
-    }
-  ]);
-};
+        host: "127.0.0.1",
+        user: "root",
+        password: "Mustangs1!",
+        database: "company_db",
+    },
+    console.log("Connected to the company_db database.")
+);
+
+inquirer.prompt([
+  {
+    type: "list",
+    message: "What would you like to do? ",
+    choices: [
+      "View all departments",
+      "View all roles",
+      "View all employees",
+      "Add a department",
+      "Add a role",
+      "Add an employee",
+      "Update an employee role",
+    ],
+    name: "options",
+  },
+]);
