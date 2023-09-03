73763e039499dc9ffd01ae66d1629a9abdcab1fc
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-09-03 16:01:23 -0500

    added code to inquirer workflow

--- server.js
+++ server.js
@@ -36,10 +36,20 @@ const firstQuestion = {
 };
 
 inquirer.prompt(firstQuestion).then((answers) => {
-  if (answers.options === "View all employees") {
-    console.log("success!");
-  } else {
-    console.log("failure");
+  if (answers.options === "View all departments") {
+    console.log("Show all departments!");
+  } else if (answers.options === "View all roles") {
+    console.log("Show all roles");
+  } else if (answers.options === "View all employees") {
+    console.log("Show all employees");
+  } else if (answers.options === "Add a department") {
+    console.log("Add a department");
+  } else if (answers.options === "Add a role") {
+    console.log("Add a role");
+  } else if (answers.options === "Add an employee") {
+    console.log("Add an employee");
+  } else if (answers.options === "Update an employee role") {
+    console.log("Update an employee");
   }
 });
 // Got it to successfully know when I am choosing a certain answer, need to then make it
